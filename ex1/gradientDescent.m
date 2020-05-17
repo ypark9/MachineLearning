dc9e545faa04dd8c8fab2b21d99b64829408b4b0
Yoonsoo Park <ypark9@ncsu.edu>
2020-05-17 15:50:09 -0400

    add a comment

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -17,7 +17,7 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
     
-    
+    %derivative towards feature.
     theta1 = theta(1)- 1 / m * alpha * sum(X * theta - y);
     theta2 = theta(2)- 1 / m * alpha * sum((X * theta - y).* X(:,2));
     theta(1) = theta1;
